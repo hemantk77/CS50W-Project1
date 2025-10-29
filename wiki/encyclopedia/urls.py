180b7618fe076a4ebe5e17cafbe8f90c21f2f1a2
hemantk77 <hemant11uk@gmail.com>
2025-10-29 23:09:01 +0100

    New path "search" created in urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>/", views.title_open, name="title_open")
+    path("wiki/<str:title>/", views.title_open, name="title_open"),
+    path("search/", views.search_results, name='search_view'),
 ]
