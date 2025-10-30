97bc314eb33763a505919ea33b67c00b4482e202
hemantk77 <hemant11uk@gmail.com>
2025-10-30 16:01:03 +0100

    new path "new/" created in urls.py

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.title_open, name="title_open"),
     path("search/", views.search_results, name='search_view'),
+    path("new/", views.create_page, name='create_view'),
 ]
